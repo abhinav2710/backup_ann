14b55431fce828ad919c42a674aac52d075a6706
Abhinav Garlapati <abhinav2710@gmail.com>
2015-12-10 09:20:25 +0530

    first update

---
+++ code/test_pooling.cpp
@@ -0,0 +1,12 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+using namespace cv;
+
+
+int main( int argc, const char** argv[]) {
+    Mat input_image = imread(argv[1]);
+    
+}
